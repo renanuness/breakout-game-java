75f8017e2604c82b1a9f7e4f8a71034f881e8eb9
renanuness <renan1504@gmail.com>
2026-07-02 11:09:58 -0300

    Creation of RaylibInputController to manage input reading and call commands method on Game

--- src/main/java/infra/RaylibInputController.java
+++ src/main/java/infra/RaylibInputController.java
@@ -1,4 +1,22 @@
 package infra;
 
+import application.Controller;
+
+import static com.raylib.Raylib.*;
+import static com.raylib.Raylib.IsKeyDown;
+import static com.raylib.Raylib.KEY_UP;
+
 public class RaylibInputController {
+
+    public void readInputs(Controller controller){
+        if(IsKeyDown(KEY_LEFT)){
+            controller.left_arrow();
+        }
+        if(IsKeyDown(KEY_RIGHT)){
+            controller.moveRight();
+        }
+        if(IsKeyDown(KEY_UP)){
+            controller.throwBall();
+        }
+    }
 }
